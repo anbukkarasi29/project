e7d8be0720724ec1991c2a15fa33b1a1dc196caf
anbukarasi29 <anbushan22@gmail.com>
2026-06-16 19:00:29 +0530

    flipcto amazon

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -1,5 +1,5 @@
 import{test} from "@playwright/test";
 test("validate flipcKart Application", async( {page} ) => {
 
-    await page.goto("https://www.flipkart.com/");
+ //   await page.goto("https://www.amazon.in/");
 })
