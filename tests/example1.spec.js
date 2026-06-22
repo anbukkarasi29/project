2c75f0fae8546fb8d4a344611bb13f68f595315f
anbukarasi29 <anbushan22@gmail.com>
2026-06-22 09:34:32 +0530

    file

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -2,4 +2,5 @@ import{test} from '@playwright/test';
 test("validate facebook Application", async( {page} ) => {
     await page.goto("https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F");
     console.log("anbu")
+    console.log("welcome")
 } )
